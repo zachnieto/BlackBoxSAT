8a07e67a502f69755a5a9bda5703923b2a4203ae
Zach Nieto <nieto.z@northeastern.edu>
2020-10-30 23:26:05 -0400

    Created Direction enum

---
+++ src/Direction.java
@@ -0,0 +1,24 @@
+public enum Direction {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST;
+
+    public Direction nextClockwiseDirection() {
+        return switch (this) {
+            case NORTH -> EAST;
+            case EAST -> SOUTH;
+            case SOUTH -> WEST;
+            case WEST -> NORTH;
+        };
+    }
+
+    public Direction nextCounterClockwiseDirection() {
+        return switch (this) {
+            case NORTH -> WEST;
+            case EAST -> NORTH;
+            case SOUTH -> EAST;
+            case WEST -> SOUTH;
+        };
+    }
+}
