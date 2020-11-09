92aa696c96c2a7e73a9e3c91520aa4440d5f80b9
bndaquino <44763346+bndaquino@users.noreply.github.com>
2020-11-09 14:01:19 -0500

    Comments for Hit
    Some of the expressions here differ slightly from the corresponding expressions in Section 3.5.

--- src/Hit.java
+++ src/Hit.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 /**
- * This class represents a ray that Hits a ball.
+ * This class represents a ray that Hits an atom.
  */
 public class Hit extends AHint {
 
@@ -16,37 +16,43 @@ public class Hit extends AHint {
       super(boardDim, p, dir);
     }
     
-    @Override
+    	// Generate the boolean expression that determines whether or not this Hit hint is satisfied given some set of atoms (entering from the border--Section 3.5)
 	public String generate() {
-    	if(this.outOfBounds(this.dir.ballCCW(this.position))) {
-    		return String.format("(~P%d%d) & (P%d%d | ", this.dir.ballCW(this.position).getX(),
-    											 	  	this.dir.ballCW(this.position).getY(), 
-    											 	  	this.dir.getNextPosn(this.position).getX(),
-    											 	  	this.dir.getNextPosn(this.position).getY())
+	// Corner case: if the CCW atom is out of bounds, then there must be no CW atom to avoid border reflection
+    	if(this.outOfBounds(this.dir.atomCCW(this.position))) {
+    		return String.format("(~P%d%d) & (P%d%d | ", 
+				     this.dir.atomCW(this.position).getX(),
+				     this.dir.atomCW(this.position).getY(), 
+				     this.dir.getNextPosn(this.position).getX(),
+				     this.dir.getNextPosn(this.position).getY())
     			+ new Hit(this.boardDim, this.dir.getNextPosn(this.position), this.dir).generate(new ArrayList<>())
     			+ ")"; 	
     	}
-    	else if(this.outOfBounds(this.dir.ballCW(this.position))) {
-    		return String.format("(~P%d%d) & (P%d%d | ", this.dir.ballCCW(this.position).getX(),
-    											 		 this.dir.ballCCW(this.position).getY(),
-    											 		 this.dir.getNextPosn(this.position).getX(),
-    											 	  	 this.dir.getNextPosn(this.position).getY())
+	// Corner case: if the CW atom is out of bounds, then there must be no CCW atom to avoid border reflection
+    	else if(this.outOfBounds(this.dir.atomCW(this.position))) {
+    		return String.format("(~P%d%d) & (P%d%d | ",
+				     this.dir.atomCCW(this.position).getX(),
+				     this.dir.atomCCW(this.position).getY(),
+				     this.dir.getNextPosn(this.position).getX(),
+				     this.dir.getNextPosn(this.position).getY())
         			+ new Hit(this.boardDim, this.dir.getNextPosn(this.position), this.dir).generate(new ArrayList<>())
         			+ ")"; 
     	}
+	// Normal entry case: there must be neither a CW atom nor a CCW atom to avoid border reflection
     	else {
-	    	return String.format("(~P%d%d) & (~P%d%d) & (P%d%d | ", this.dir.ballCW(this.position).getX(),
-	    														 	this.dir.ballCW(this.position).getY(),
-	    														 	this.dir.ballCCW(this.position).getX(),
-	    														 	this.dir.ballCCW(this.position).getY(),
-	    														 	this.dir.getNextPosn(this.position).getX(),
-	    	    											 	  	this.dir.getNextPosn(this.position).getY())
+	    	return String.format("(~P%d%d) & (~P%d%d) & (P%d%d | ",
+				     this.dir.atomCW(this.position).getX(),
+				     this.dir.atomCW(this.position).getY(),
+				     this.dir.atomCCW(this.position).getX(),
+				     this.dir.atomCCW(this.position).getY(),
+				     this.dir.getNextPosn(this.position).getX(),
+				     this.dir.getNextPosn(this.position).getY())
 	    		   + new Hit(this.boardDim, this.dir.getNextPosn(this.position), this.dir).generate(new ArrayList<>())
 	    		   + ")";
-    	}
+	}
 	}
 
-	@Override
+	// Continue generating the expression for a ray that has already entered the board (Section 3.3)
 	public String generate(ArrayList<AHint> checked) {
 	    if (checked.contains(this)) {
 	      return AHint.nil;
@@ -54,67 +60,75 @@ public class Hit extends AHint {
 	    
 	    checked.add(this);
 
-	    // get the position of a ball that would deflect the ray from clockwise
-	    Posn ballCW = this.dir.ballCW(this.position);
-	    // get the position of a ball that would deflect the ray from counterclockwise
-	    Posn ballCCW = this.dir.ballCCW(this.position);
+	    // Get the position of an atom that would deflect the ray clockwise
+	    Posn atomCW = this.dir.atomCW(this.position);
+	    // Get the position of an atom that would deflect the ray counterclockwise
+	    Posn atomCCW = this.dir.atomCCW(this.position);
 	    
-		if(this.outOfBounds(ballCW) && this.outOfBounds(ballCCW))
+		// First check the border cases (Section 3.6): towards the border, going clockwise along the border, and going counterclockwise along the border (Section 3.6)
+		// If the ray is about to exit the board,
+		if(this.outOfBounds(atomCW) && this.outOfBounds(atomCCW))
+			// then the only way for the hit to be satisfied is for there to be an atom at the current position.
 			return "P" + this.position.getX() + this.position.getY();
-		else if ( this.outOfBounds(ballCW)  ) {
+		// If the ray would exit on a counterclockwise deflection,
+		else if ( this.outOfBounds(atomCW)  ) {
+			// then the hit could be satisfied with an atom here, or with no counterclockwise deflection and an atom later.
 			return String.format("(P%d%d | ((P%d%d => " + AHint.nil + ") & (~P%d%d => ",
 						this.dir.getNextPosn(this.position).getX(),
 						this.dir.getNextPosn(this.position).getY(),
-						ballCCW.getX(), 
-						ballCCW.getY(), 
-						ballCCW.getX(),
-						ballCCW.getY())
+						atomCCW.getX(), 
+						atomCCW.getY(), 
+						atomCCW.getX(),
+						atomCCW.getY())
 					+ new Hit(this.boardDim, this.dir.getNextPosn(this.position), this.dir).generate(checked) + ")))";
 		}
-		else if ( this.outOfBounds(ballCCW) ) {
+		// If the ray would exit on a clockwise deflection,
+		else if ( this.outOfBounds(atomCCW) ) {
+			// then the hit could be satisfied with an atom here, or with no clockwise deflection and an atom later.
 			return String.format("(P%d%d | ((P%d%d => " + AHint.nil + ") & (~P%d%d => ",
 						this.dir.getNextPosn(this.position).getX(),
 						this.dir.getNextPosn(this.position).getY(),
-						ballCW.getX(),
-						ballCW.getY(), 
-						ballCW.getX(),
-						ballCW.getY())
+						atomCW.getX(),
+						atomCW.getY(), 
+						atomCW.getX(),
+						atomCW.getY())
 				+ new Hit(this.boardDim, this.dir.getNextPosn(this.position), this.dir).generate(checked) + ")))";
-		
-		}		
+		}
+		// If the ray is somewhere in the middle of the board, then check for an atom here or an atom after the ray continues in the deflected direction.
 		else {			
-			return String.format("(P%d%d | ((~P%d%d & ~P%d%d) => ", 
+			// Case 1: no deflection
+			return String.format("(P%d%d | ((~P%d%d & ~P%d%d) => ",
 					this.dir.getNextPosn(this.position).getX(),
 			 	  	this.dir.getNextPosn(this.position).getY(),
-					ballCW.getX(),
-					ballCW.getY(),
-					ballCCW.getX(),
-					ballCCW.getY())
+					atomCW.getX(),
+					atomCW.getY(),
+					atomCCW.getX(),
+					atomCCW.getY())
 				+ new Hit(this.boardDim, dir.getNextPosn(this.position), this.dir).generate(new ArrayList<>(checked))
 				+ ")"
-				
+				// Case 2: clockwise deflection
 				+ String.format(" & ((P%d%d & ~P%d%d) => ",
-						ballCW.getX(),
-						ballCW.getY(), 
-						ballCCW.getX(),
-						ballCCW.getY())
+						atomCW.getX(),
+						atomCW.getY(), 
+						atomCCW.getX(),
+						atomCCW.getY())
 				+ new Hit(this.boardDim, this.dir.nextClockwiseDirection().getNextPosn(this.position), this.dir.nextClockwiseDirection()).generate(new ArrayList<>(checked))
 				+ ")"
-				
+				// Case 3: counterclockwise deflection
 				+ String.format(" & ((~P%d%d & P%d%d) => ",
-						ballCW.getX(),
-						ballCW.getY(), 
-						ballCCW.getX(),
-						ballCCW.getY())
+						atomCW.getX(),
+						atomCW.getY(), 
+						atomCCW.getX(),
+						atomCCW.getY())
 				+ new Hit(this.boardDim, this.dir.nextCounterClockwiseDirection().getNextPosn(this.position), this.dir.nextCounterClockwiseDirection()).generate(new ArrayList<>(checked))
 				+ ")"
+				// Case 4: refelection immediately means the Hit is unsatisfied
 				+ String.format(" & ((P%d%d & P%d%d) => " + AHint.nil + ")",
-						ballCW.getX(),
-						ballCW.getY(), 
-						ballCCW.getX(),
-						ballCCW.getY())
+						atomCW.getX(),
+						atomCW.getY(), 
+						atomCCW.getX(),
+						atomCCW.getY())
 				+ ")"; 
 		}
-	
 	}
 }
